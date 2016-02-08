62b7715f29445168b09889f5c5b22e9ac0665cb7
neyogiry <yrigoyen.fernando@gmail.com>
2016-02-08 12:51:19 -0500

    Update Hello_Neyo

---
+++ Hello_Neyo.java
@@ -0,0 +1,5 @@
+public class Hello_Neyo {
+	public static void main(String args[]){
+		System.out.print("Hello Neyo!");
+	}
+}
--- Hola_Neyo.java
+++
@@ -1,5 +0,0 @@
-public class Hola_Mundo {
-	public static void main(String args[]){
-		System.out.print("Hola Neyo!");
-	}
-}
